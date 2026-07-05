c1a4e866602d17c2c7f77a8ef894742668c13338
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2026-07-05 14:26:53 -0300

    authenticatedShouldReturnUserEntityWhenUserExists

--- Desafio DSMovie Jacoco/src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ Desafio DSMovie Jacoco/src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,20 +1,54 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import com.devsuperior.dsmovie.utils.CustomUserUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
+import java.util.Optional;
+
+@ExtendWith({SpringExtension.class, MockitoExtension.class})
 @ContextConfiguration
 public class UserServiceTests {
 
 	@InjectMocks
 	private UserService service;
 
+	@Mock
+	private UserRepository repository;
+
+	@Mock
+	private CustomUserUtil userUtil;
+
+	private String existingUsername;
+
+	private UserEntity userEntity;
+
+	@BeforeEach
+	void setUp(){
+		existingUsername = "maria@gmail.com";
+		userEntity = UserFactory.createUserEntity();
+
+		Mockito.lenient().when(repository.findByUsername(existingUsername)).thenReturn(Optional.of(userEntity));
+	}
+
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
+		Mockito.lenient().when(userUtil.getLoggedUsername()).thenReturn(existingUsername);
+		UserEntity result = service.authenticated();
+
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getUsername(), existingUsername);
 	}
 
 	@Test
